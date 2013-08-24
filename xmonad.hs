3cb9b72b2818068a003bb2ec10b35741e1f7ca5e
Chris Van Horne <chris.vanhorne@gmail.com>
2013-08-24 15:26:01 -0700

    Debian stable Haskell package support

--- xmonad.hs
+++ xmonad.hs
@@ -3,12 +3,16 @@ import Data.Monoid (mappend)
 import System.IO (hPutStrLn)
 
 import XMonad
-import XMonad.Actions.Volume
+--import XMonad.Actions.Volume
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig
 import XMonad.Util.Run (spawnPipe)
 import qualified XMonad.StackSet as W
+
+winKey = mod4Mask
+altKey = mod1Mask
+volumeStep = 4
 
 main = do
     xmobar <- spawnPipe "xmobar"
@@ -19,25 +23,22 @@ main = do
                         { ppOutput = hPutStrLn xmobar
                         , ppTitle = xmobarColor "green" "" . shorten 50
                         }
+        , terminal = "urxvt"
         }
         `additionalKeys` myKeys
-
-winKey = mod4Mask
-altKey = mod1Mask
-volumeStep = 4
 
 myKeys =
     [ ((winKey, xK_s), spawn "systemctl suspend")
 
     -- applications
-    , ((winKey, xK_c), spawn "chromium")
-    , ((winKey .|. shiftMask, xK_c), spawn "chromium --incognito")
+    --, ((winKey, xK_c), spawn "chromium")
+    --, ((winKey .|. shiftMask, xK_c), spawn "chromium --incognito")
     , ((winKey, xK_f), spawn "firefox")
 
     -- audio management
-    , ((winKey, xK_F5), getMute >>= setMute . not >> return ())
-    , ((winKey, xK_F6), setMute False >> lowerVolume volumeStep >> return ())
-    , ((winKey, xK_F7), setMute False >> raiseVolume volumeStep >> return ())
+    --, ((winKey, xK_F5), getMute >>= setMute . not >> return ())
+    --, ((winKey, xK_F6), setMute False >> lowerVolume volumeStep >> return ())
+    --, ((winKey, xK_F7), setMute False >> raiseVolume volumeStep >> return ())
     ]
 
 myManageHook = composeAll
